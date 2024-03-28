4cb43c69b7c521905c8f91ca7f4fbca439912c59
aditheking <aupreti72@gmail.com>
2024-03-28 14:41:34 +0530

    Fetch profile name from Firestore in Profile_Activity
    - Added Firebase Firestore and Firebase Authentication initialization
    - Retrieved user's name from Firestore based on the current user's ID
    - Set the retrieved name to the profile name TextView
    Signed-off-by: aditheking <aupreti72@gmail.com>

--- app/src/main/java/com.mini/amimatch/Profile_Activity.kt
+++ app/src/main/java/com.mini/amimatch/Profile_Activity.kt
@@ -8,15 +8,18 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx
-
-
 
 class Profile_Activity : AppCompatActivity() {
     private val mContext: Context = this@Profile_Activity
     private var imagePerson: ImageView? = null
     private var name: TextView? = null
     private val userId: String? = null
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate: create the page")
         super.onCreate(savedInstanceState)
@@ -26,6 +29,27 @@ class Profile_Activity : AppCompatActivity() {
         setupTopNavigationView()
         imagePerson = findViewById<ImageView>(R.id.circle_profile_image)
         name = findViewById<TextView>(R.id.profile_name)
+
+        mAuth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        val currentUserId = mAuth.currentUser?.uid
+
+        currentUserId?.let { userId ->
+            firestore.collection("users").document(userId).get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+                        val userName = document.getString("name")
+                        name?.text = userName
+                    } else {
+                        Log.d(TAG, "No such document")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.d(TAG, "get failed with ", exception)
+                }
+        }
+
         val edit_btn = findViewById<ImageButton>(R.id.edit_profile)
         edit_btn.setOnClickListener {
             val intent = Intent(
@@ -56,7 +80,7 @@ class Profile_Activity : AppCompatActivity() {
         TopNavigationViewHelper.enableNavigation(mContext, tvEx)
         val menu = tvEx.menu
         val menuItem = menu.getItem(ACTIVITY_NUM)
-        menuItem.setChecked(true)
+        menuItem.isChecked = true
     }
 
     companion object {
